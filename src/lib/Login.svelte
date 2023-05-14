9499f95e1ff792edf3a773a8e692e3595e11baa9
Corsodix <corsodix@gmail.com>
2023-05-14 20:30:05 +0400

    add login component

---
+++ src/lib/Login.svelte
@@ -0,0 +1,28 @@
+<script>
+  import {
+    onAuthStateChanged,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+  } from "firebase/auth";
+  import { auth } from "./firebase";
+
+  async function signIn() {
+    var provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider);
+  }
+  function signOutUser() {
+    signOut(auth);
+  }
+  import { userStore } from "sveltefire";
+
+  const user = userStore(auth);
+</script>
+
+{#if $user}
+  <p>Hi {$user.uid}</p>
+  <button on:click={signOutUser}>Logout</button>
+{:else}
+  <p>Sign in...</p>
+  <button on:click={signIn}>Login</button>
+{/if}
